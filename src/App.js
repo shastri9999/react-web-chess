e6ab741af2c83e4d27500f01958bad95bf19bd24
Shastri Subramanyam <shastri.subramanyam@indiez.io>
2018-02-22 00:50:00 +0530

    Added modern-normalize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // @flow
 import * as React from "react";
+import "modern-normalize/modern-normalize.css";
 import { Main } from "./App.style";
 
 class App extends React.Component<{}> {
   render() {
     return (
       <Main>
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>Welcome to React</h1>
         </header>
       </Main>
     );
